b28d2fc7e61795d0d9f9a2d7f79384b999e55ed5
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-05-16 20:39:47 -0400

    add score support for multiple target

---
+++ Assets/Scripts/HitDetector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetector : MonoBehaviour {
+
+	ScoreKeeper scoreKeeper;
+	public int hitPoint = 1;
+
+	void Start () {
+		scoreKeeper = FindObjectOfType<ScoreKeeper>();
+	}
+	
+	void OnCollisionEnter(Collision collison){
+		scoreKeeper.incrementScore (hitPoint);
+	}
+}

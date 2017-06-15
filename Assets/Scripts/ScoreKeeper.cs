b1f30f8dd0d0b27fe6f887e4037124a26ae623c9
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-06-14 21:00:13 -0400

    add time event, score and time remain ui

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour {
-	int score = 0 ;
+	public int score = 0 ;
 
 	public void incrementScore(int point){
 		score += point;
-		Debug.Log ("Score : " + score);
 	}
 }
---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDisplay : MonoBehaviour {
+
+	LevelManager levelManager;
+	Text text;
+	void Start () {
+		levelManager = FindObjectOfType<LevelManager> ();
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Time Remaining:" + levelManager.timeRemaining;
+	}
+}

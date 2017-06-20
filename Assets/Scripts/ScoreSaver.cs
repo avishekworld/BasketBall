3faf1828d8243f0825377ed514b15dffebb02bae
Avishek's Macbook <avishek.ahmed@gmail.com>
2017-06-19 22:07:02 -0400

    save score for game over screen + convert canvas into world space mode

---
+++ Assets/Scripts/ScoreSaver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSaver : MonoBehaviour {
+
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+		ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper> ();
+		if (scoreKeeper) {
+			score = scoreKeeper.score;
+			Destroy (scoreKeeper.gameObject);
+		}
+
+	}
+
+}
